ba405b818e8a2ea3c71aee8d34efe7fb046a40a9
Andrew <125866011+ArchAndrew@users.noreply.github.com>
2024-02-11 00:47:02 -0500

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "You have now left The Matrix, " + Congradulations + "!\n"
+    message = "You have now left The Matrix, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
