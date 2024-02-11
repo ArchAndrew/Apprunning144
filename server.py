d3c15693fe7e75eaf1d17f05f2273a3742bce586
Andrew <125866011+ArchAndrew@users.noreply.github.com>
2024-02-11 11:13:38 -0500

    Update server.py

--- server.py
+++ server.py
@@ -4,17 +4,17 @@ from pyramid.response import Response
 import os
 
 def hello_world(request):
-    Andrew = os.environ.get('ANDREW')
-    if Andrew == None or len(Andrew) == 0:
-        Andrew = "world"
-    message = "You have now left The Matrix, " + Andrew + "!\n"
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
     return Response(message)
 
-if __Andrew__ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
         config.add_route('hello', '/')
-        config.add_view(hello_world, route_Andrew='hello')
+        config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
